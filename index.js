8601ecfaa2d0e818023be403f1c2c61b8c3c866b
matthewmjm <matthewmjm@yahoo.com>
2021-04-30 22:56:14 -0600

    setup graphQL typeDefs and resolvers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { graphqlHTTP } = require("express-graphql");
+const { makeExecutableSchema } = require("graphql-tools");
 require("dotenv").config();
 
 const uri = `mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.hj6fb.mongodb.net/${process.env.MONGO_DB_NAME}?retryWrites=true&w=majority`;
@@ -9,7 +11,38 @@ const port = process.env.PORT || 4000;
 
 mongoose
 	.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-	.then(() => console.log("MongoDB connected!!"))
+	.then(() => console.log("MongoDB connected!! 🤘"))
 	.catch((error) => console.error(error));
+
+const typeDefs = `
+	type Query {
+		flowers: [Flower]
+	}
+
+	type Flower {
+		_id: ID!
+		kind: String
+		petals: Int
+	}
+`;
+
+const resolvers = {
+	Query: {
+		flowers: () => [{ _id: 1, kind: "cool", petals: 100 }],
+	},
+};
+
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers,
+});
+
+app.use(
+	"/graphql",
+	graphqlHTTP({
+		schema,
+		graphiql: true,
+	})
+);
 
 app.listen(port, () => console.log(`listening on port ${port}`));
